03b07983030f1b458864aef99bd9fffd644e0963
Paulo Sérgio Silva Amaro <paulo_amaro_94@hotmail.com>
2015-12-09 20:18:36 +0000

    9/12 20:18

---
+++ Ideal/Classes/Ideia.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ideal.Classes
+{
+    public class Ideia
+    {
+        public enum LEstado { Pendente, EmAnalise, AImplementar, Implementado, Descartado };
+
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime Data { get; set; }
+        public bool Partilha { get; set; }
+        private int _estado;
+        public int Estado
+        {
+            get
+            {
+                return _estado;
+            }
+            set
+            {
+                // Confirmar que o estado pertence à lista de estados, caso contrário usa a predefinição (1º estado)
+                _estado = value < Enum.GetNames(typeof(LEstado)).Length ? value : 0;
+            }
+        }
+
+        public Ideia()
+        {
+            Titulo = "Erro";
+            Conteudo = "Erro";
+            Data = DateTime.Now;
+            Partilha = false;
+            Estado = (int)LEstado.Pendente;
+        }
+
+        public Ideia(string tit, string con)
+        {
+            Titulo = tit;
+            Conteudo = con;
+            Data = DateTime.Now;
+            Partilha = false;
+            Estado = (int)LEstado.Pendente;
+        }
+
+        public void Partilhar()
+        {
+            // Cria uma ligação de partilha da ideia
+            Partilha = true;
+        }
+
+        public void MudaEstado(LEstado novo)
+        {
+            Estado = (int)novo;
+        }
+    }
+}
---
+++ IdealTeste/IdeiaTeste.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ideal;
+using Ideal.Classes;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace IdealTeste
+{
+    [TestClass]
+    public class IdeiaTeste
+    {
+        private bool Data(DateTime data)
+        {
+            // Para o caso do teste ser feito em cima da viragem do segundo (Procurar melhor maneira de testar a data)
+            int segundos = DateTime.Now.Second;
+            if (segundos == 0)
+                segundos = 60;
+            return (segundos - data.Second <= 1);
+        }
+
+        [TestMethod]
+        public void IdeiaVaziaTeste()
+        {
+            Ideia ideia = new Ideia();
+
+            Assert.AreEqual("Erro", ideia.Titulo);
+            Assert.AreEqual("Erro", ideia.Conteudo);
+            Assert.AreEqual(Data(ideia.Data), true);
+            Assert.AreEqual(ideia.Partilha, false);
+            Assert.AreEqual(ideia.Estado, (int)Ideia.LEstado.Pendente);
+        }
+
+        [TestMethod]
+        public void IdeiaSimplesTeste()
+        {
+            Ideia ideia = new Ideia("Ideia", "Conteúdo da ideia");
+
+            Assert.AreEqual("Ideia", ideia.Titulo);
+            Assert.AreEqual("Conteúdo da ideia", ideia.Conteudo);
+            Assert.AreEqual(Data(ideia.Data), true);
+            Assert.AreEqual(ideia.Partilha, false);
+            Assert.AreEqual(ideia.Estado, (int)Ideia.LEstado.Pendente);
+        }
+
+        [TestMethod]
+        public void PartilhaIdeiaTeste()
+        {
+            Ideia ideia = new Ideia("Ideia", "Conteúdo da ideia");
+
+            ideia.Partilhar();
+
+            Assert.AreEqual(ideia.Partilha, true);
+        }
+
+        [TestMethod]
+        public void MudaEstadoIdeiaTeste()
+        {
+            Ideia ideia = new Ideia("Ideia", "Conteúdo da ideia");
+
+            Assert.AreEqual(ideia.Estado, (int)Ideia.LEstado.Pendente);
+
+            ideia.MudaEstado(Ideia.LEstado.EmAnalise);
+            Assert.AreEqual(ideia.Estado, (int)Ideia.LEstado.EmAnalise);
+
+            ideia.MudaEstado(Ideia.LEstado.AImplementar);
+            Assert.AreEqual(ideia.Estado, (int)Ideia.LEstado.AImplementar);
+
+            ideia.MudaEstado(Ideia.LEstado.Implementado);
+            Assert.AreEqual(ideia.Estado, (int)Ideia.LEstado.Implementado);
+
+            ideia.MudaEstado(Ideia.LEstado.Descartado);
+            Assert.AreEqual(ideia.Estado, (int)Ideia.LEstado.Descartado);
+        }
+
+        [TestMethod]
+        public void EditaIdeiaTeste()
+        {
+            Ideia ideia = new Ideia("Ideia", "Conteúdo da ideia");
+
+            ideia.Titulo = "Ideia modificada";
+            ideia.Conteudo = "Conteúdo modificado";
+
+            Assert.AreEqual(ideia.Titulo, "Ideia modificada");
+            Assert.AreEqual(ideia.Conteudo, "Conteúdo modificado");
+        }
+    }
+}
